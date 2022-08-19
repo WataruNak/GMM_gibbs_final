9b503d8e9117760cf895bb5d25719faa7a9f6171
wat_com <wn.wolke@me.com>
2022-08-19 16:17:47 +0900

    220819

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ SESSION_CONFIGS = [
             'cat_final_result',
             'questionnaires'
             ],
-        num_demo_participants=120,
+        num_demo_participants=110,
     ),
     dict(
         name='categorize_and_brief_questionnaire',
